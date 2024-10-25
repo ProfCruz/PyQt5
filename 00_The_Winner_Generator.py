0c1e0d6c9a8a4d4673bf3c877e1971c06dc6c0aa
Renee Cruz <129509588+reneecruzpt@users.noreply.github.com>
2024-10-25 20:46:28 +0100

    Título e Comentários

--- 00_The_Winner_Generator.py
+++ 00_The_Winner_Generator.py
@@ -1,3 +1,6 @@
+'''Basics of PyQt5
+Dev: Renee Cruz'''
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 from random import randint
